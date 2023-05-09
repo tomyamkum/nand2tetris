38bcf6774f9cffa3a97f62ef540b623f9921340e
Tomoya Yamashita <tomoya@TomoyanoMacBook-Pro-2.local>
2023-05-09 22:29:46 +0900

    multi input

--- src/common/gate.rs
+++ src/common/gate.rs
@@ -7,7 +7,24 @@ pub fn not(a: bool) -> bool {
 }
 
 pub fn not16(a: [bool; 16]) -> [bool; 16] {
-    [not(a[0]), not(a[1]), not(a[2]), not(a[3]), not(a[4]), not(a[5]), not(a[6]), not(a[7]), not(a[8]), not(a[9]), not(a[10]), not(a[11]), not(a[12]), not(a[13]), not(a[14]), not(a[15])]
+    [
+        not(a[0]),
+        not(a[1]),
+        not(a[2]),
+        not(a[3]),
+        not(a[4]),
+        not(a[5]),
+        not(a[6]),
+        not(a[7]),
+        not(a[8]),
+        not(a[9]),
+        not(a[10]),
+        not(a[11]),
+        not(a[12]),
+        not(a[13]),
+        not(a[14]),
+        not(a[15]),
+    ]
 }
 
 pub fn and(a: bool, b: bool) -> bool {
@@ -15,7 +32,24 @@ pub fn and(a: bool, b: bool) -> bool {
 }
 
 pub fn and16(a: [bool; 16], b: [bool; 16]) -> [bool; 16] {
-    [and(a[0], b[0]), and(a[1], b[1]), and(a[2], b[2]), and(a[3], b[3]), and(a[4], b[4]), and(a[5], b[5]), and(a[6], b[6]), and(a[7], b[7]), and(a[8], b[8]), and(a[9], b[9]), and(a[10], b[10]), and(a[11], b[11]), and(a[12], b[12]), and(a[13], b[13]), and(a[14], b[14]), and(a[15], b[15])]
+    [
+        and(a[0], b[0]),
+        and(a[1], b[1]),
+        and(a[2], b[2]),
+        and(a[3], b[3]),
+        and(a[4], b[4]),
+        and(a[5], b[5]),
+        and(a[6], b[6]),
+        and(a[7], b[7]),
+        and(a[8], b[8]),
+        and(a[9], b[9]),
+        and(a[10], b[10]),
+        and(a[11], b[11]),
+        and(a[12], b[12]),
+        and(a[13], b[13]),
+        and(a[14], b[14]),
+        and(a[15], b[15]),
+    ]
 }
 
 pub fn or(a: bool, b: bool) -> bool {
@@ -23,7 +57,29 @@ pub fn or(a: bool, b: bool) -> bool {
 }
 
 pub fn or16(a: [bool; 16], b: [bool; 16]) -> [bool; 16] {
-    [or(a[0], b[0]), or(a[1], b[1]), or(a[2], b[2]), or(a[3], b[3]), or(a[4], b[4]), or(a[5], b[5]), or(a[6], b[6]), or(a[7], b[7]), or(a[8], b[8]), or(a[9], b[9]), or(a[10], b[10]), or(a[11], b[11]), or(a[12], b[12]), or(a[13], b[13]), or(a[14], b[14]), or(a[15], b[15])]
+    [
+        or(a[0], b[0]),
+        or(a[1], b[1]),
+        or(a[2], b[2]),
+        or(a[3], b[3]),
+        or(a[4], b[4]),
+        or(a[5], b[5]),
+        or(a[6], b[6]),
+        or(a[7], b[7]),
+        or(a[8], b[8]),
+        or(a[9], b[9]),
+        or(a[10], b[10]),
+        or(a[11], b[11]),
+        or(a[12], b[12]),
+        or(a[13], b[13]),
+        or(a[14], b[14]),
+        or(a[15], b[15]),
+    ]
+}
+
+#[rustfmt::skip]
+pub fn or8way(a: [bool; 8]) -> bool {
+    or(or(or(or(or(or(or(a[0], a[1]), a[2]), a[3]), a[4]), a[5]), a[6]), a[7])
 }
 
 pub fn xor(a: bool, b: bool) -> bool {
@@ -35,9 +91,40 @@ pub fn mux(a: bool, b: bool, sel: bool) -> bool {
 }
 
 pub fn mux16(a: [bool; 16], b: [bool; 16], sel: bool) -> [bool; 16] {
-    [mux(a[0], b[0], sel), mux(a[1], b[1], sel), mux(a[2], b[2], sel), mux(a[3], b[3], sel), mux(a[4], b[4], sel), mux(a[5], b[5], sel), mux(a[6], b[6], sel), mux(a[7], b[7], sel), mux(a[8], b[8], sel), mux(a[9], b[9], sel), mux(a[10], b[10], sel), mux(a[11], b[11], sel), mux(a[12], b[12], sel), mux(a[13], b[13], sel), mux(a[14], b[14], sel), mux(a[15], b[15], sel)]
+    [
+        mux(a[0], b[0], sel),
+        mux(a[1], b[1], sel),
+        mux(a[2], b[2], sel),
+        mux(a[3], b[3], sel),
+        mux(a[4], b[4], sel),
+        mux(a[5], b[5], sel),
+        mux(a[6], b[6], sel),
+        mux(a[7], b[7], sel),
+        mux(a[8], b[8], sel),
+        mux(a[9], b[9], sel),
+        mux(a[10], b[10], sel),
+        mux(a[11], b[11], sel),
+        mux(a[12], b[12], sel),
+        mux(a[13], b[13], sel),
+        mux(a[14], b[14], sel),
+        mux(a[15], b[15], sel),
+    ]
 }
 
-pub fn dmux(input: bool, sel: bool) -> (bool, bool) {
-  (and(input, not(sel)), and(input, sel))
+#[rustfmt::skip]
+pub fn mux4way16(a: [bool; 16], b: [bool; 16], c: [bool; 16], d: [bool; 16], sel: [bool; 2]) -> [bool; 16] {
+    mux16(mux16(a, b, sel[0]), mux16(c, d, sel[0]), sel[1])
 }
+
+#[rustfmt::skip]
+pub fn mux8way16(a: [bool; 16], b: [bool; 16], c: [bool; 16], d: [bool; 16], e: [bool; 16], f: [bool; 16], g: [bool; 16], h: [bool; 16], sel: [bool; 3]) -> [bool; 16] {
+    mux16(mux4way16(a, b, c, d, [sel[0], sel[1]]), mux4way16(e, f, g, h, [sel[0], sel[1]]), sel[2])
+}
+
+pub fn dmux(input: bool, sel: bool) -> [bool; 2] {
+    [and(input, not(sel)), and(input, sel)]
+}
+
+//pub fn dmux4way(input: bool, sel: [bool; 2]) -> [bool; 4] {
+//  []
+//}
--- tests/test.rs
+++ tests/test.rs
@@ -3,73 +3,109 @@ use nand2tetris::common::gate;
 
 #[test]
 fn nand_test() {
-  assert_eq!(false, gate::nand(true, true));
-  assert_eq!(true, gate::nand(true, false));
-  assert_eq!(true, gate::nand(false, true));
-  assert_eq!(true, gate::nand(false, false));
+    assert_eq!(false, gate::nand(true, true));
+    assert_eq!(true, gate::nand(true, false));
+    assert_eq!(true, gate::nand(false, true));
+    assert_eq!(true, gate::nand(false, false));
 }
 
 #[test]
 fn not_test() {
-  assert_eq!(true, gate::not(false));
-  assert_eq!(false, gate::not(true));
+    assert_eq!(true, gate::not(false));
+    assert_eq!(false, gate::not(true));
 }
 
 #[test]
 fn and_test() {
-  assert_eq!(true, gate::and(true, true));
-  assert_eq!(false, gate::and(true, false));
-  assert_eq!(false, gate::and(false, true));
-  assert_eq!(false, gate::and(false, false));
+    assert_eq!(true, gate::and(true, true));
+    assert_eq!(false, gate::and(true, false));
+    assert_eq!(false, gate::and(false, true));
+    assert_eq!(false, gate::and(false, false));
 }
 
 #[test]
 fn or_test() {
-  assert_eq!(true, gate::or(true, true));
-  assert_eq!(true, gate::or(true, false));
-  assert_eq!(true, gate::or(false, true));
-  assert_eq!(false, gate::or(false, false));
+    assert_eq!(true, gate::or(true, true));
+    assert_eq!(true, gate::or(true, false));
+    assert_eq!(true, gate::or(false, true));
+    assert_eq!(false, gate::or(false, false));
 }
 
 #[test]
 fn xor_test() {
-  assert_eq!(false, gate::xor(true, true));
-  assert_eq!(true, gate::xor(true, false));
-  assert_eq!(true, gate::xor(false, true));
-  assert_eq!(false, gate::xor(false, false));
+    assert_eq!(false, gate::xor(true, true));
+    assert_eq!(true, gate::xor(true, false));
+    assert_eq!(true, gate::xor(false, true));
+    assert_eq!(false, gate::xor(false, false));
 }
 
 #[test]
 fn mux_test() {
-  for a in [true, false] {
-    for b in [true, false] {
-      for sel in [true, false] {
-        if sel {
-          assert_eq!(b, gate::mux(a, b, sel));
-        } else {
-          assert_eq!(a, gate::mux(a, b, sel));
+    for a in [true, false] {
+        for b in [true, false] {
+            for sel in [true, false] {
+                if sel {
+                    assert_eq!(b, gate::mux(a, b, sel));
+                } else {
+                    assert_eq!(a, gate::mux(a, b, sel));
+                }
+            }
         }
-      }
     }
-  }
 }
 
 #[test]
 fn dmux_test() {
-  for input in [true, false] {
-    for sel in [true, false] {
-      if sel {
-        assert_eq!((false, input), gate::dmux(input, sel));
-      }
-      else {
-        assert_eq!((input, false), gate::dmux(input, sel));
-      }
+    for input in [true, false] {
+        for sel in [true, false] {
+            if sel {
+                assert_eq!((false, input), gate::dmux(input, sel));
+            } else {
+                assert_eq!((input, false), gate::dmux(input, sel));
+            }
+        }
     }
-  }
 }
 
 #[test]
+#[rustfmt::skip]
 fn not16_test() {
-  let a:[bool; 16] = [true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false];
-  assert_eq!([false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true], gate::not16(a));
+    let a: [bool; 16] = [true, true, true, true, true, true, true, true, false, false, false, false, false, false, false, false];
+    assert_eq!([false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true], gate::not16(a));
+}
+
+#[test]
+#[rustfmt::skip]
+fn mux4way16_test() {
+    let a: [bool; 16] = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+    let b: [bool; 16] = [true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]; 
+    let c: [bool; 16] = [true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false]; 
+    let d: [bool; 16] = [true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false]; 
+
+    assert_eq!(a, gate::mux4way16(a, b, c, d, [false, false]));
+    assert_eq!(b, gate::mux4way16(a, b, c, d, [true, false]));
+    assert_eq!(c, gate::mux4way16(a, b, c, d, [false, true]));
+    assert_eq!(d, gate::mux4way16(a, b, c, d, [true, true]));
+}
+
+#[test]
+#[rustfmt::skip]
+fn mux8way16_test() {
+    let a: [bool; 16] = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+    let b: [bool; 16] = [true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]; 
+    let c: [bool; 16] = [true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false]; 
+    let d: [bool; 16] = [true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false]; 
+    let e: [bool; 16] = [true, true, true, true, false, false, false, false, false, false, false, false, false, false, false, false]; 
+    let f: [bool; 16] = [true, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false]; 
+    let g: [bool; 16] = [true, true, true, true, true, true, false, false, false, false, false, false, false, false, false, false]; 
+    let h: [bool; 16] = [true, true, true, true, true, true, true, false, false, false, false, false, false, false, false, false]; 
+
+    assert_eq!(a, gate::mux8way16(a, b, c, d, e, f, g, h, [false, false, false]));
+    assert_eq!(b, gate::mux8way16(a, b, c, d, e, f, g, h, [true, false, false]));
+    assert_eq!(c, gate::mux8way16(a, b, c, d, e, f, g, h, [false, true, false]));
+    assert_eq!(d, gate::mux8way16(a, b, c, d, e, f, g, h, [true, true, false]));
+    assert_eq!(e, gate::mux8way16(a, b, c, d, e, f, g, h, [false, false, true]));
+    assert_eq!(f, gate::mux8way16(a, b, c, d, e, f, g, h, [true, false, true]));
+    assert_eq!(g, gate::mux8way16(a, b, c, d, e, f, g, h, [false, true, true]));
+    assert_eq!(h, gate::mux8way16(a, b, c, d, e, f, g, h, [true, true, true]));
 }
